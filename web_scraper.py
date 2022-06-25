3ba76f4e323359a39ac5beb14f6a53091f9a71d3
Marina-28 <greatundeadmoose@gmail.com>
2022-06-25 18:00:52 +0200

    first request

---
+++ web_scraper.py
@@ -0,0 +1,12 @@
+import requests
+
+url = input("input the URL\n")
+r = requests.get(url)
+if r.status_code == 200:
+    jdict = r.json()
+    if 'content' in jdict.keys():
+        print(jdict['content'])
+    else:
+        print("Invalid quote resource!")
+else:
+    print("Invalid quote resource!")
